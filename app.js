41ecac84916c6bf4d5f6c32a2818c81254d8aea5
Wadim <dem_cool@mail.ru>
2016-03-16 22:29:15 +0300

    Vot tak vot

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var router = require('./router');
 var requestHandlers = require('./requestHandlers');
 
 var handle = {};
-handle['/'] = requestHandlers.loadHomePage;
+handle['/'] = requestHandlers.loadHomePage;//функции для обработки запросов
 handle['/home'] = requestHandlers.loadHomePage;
 
 server.start(router.route, handle);
