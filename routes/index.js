3ea2a10b8d435a2c6b6bfddc5843e48e6658902a
Shuba Maxim <pro100stalkerss@gmail.com>
2016-03-30 22:18:40 +0300

    try return err object

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,41 @@ var express = require('express');
 var parser = require('../parser');
 
 var router = express.Router();
-var massJson = "data lost";
+var massJson = [];
 
 parser.getData(function (err, result) {
     if (err) {
-        console.log(err.message);
-    }
-
-    else {
-
+        console.log(err.stack);
+    } else {
         massJson = result;
         console.log('Cache is built');
     }
 });
 
-setInterval(function() {
+setInterval(function () {
     parser.getData(function (err, result) {
         if (err) {
             console.log(err.message);
-        }
-
-        else {
+        } else {
             massJson = result;
             console.log('Cache is built');
         }
-
-    })
+    });
 }, 180000);
 
-router.get('/', function(req, res) {
-    res.render(("index"), {body: massJson});
+router.get('/', function (req, res) {
+    res.render('index', {body: massJson});
 });
+/*
+app.use(function(req, res, next) {
+    if(massJson.length === 0)
+    {
+        var err = new Error('No content');
+        err.status = 204;
+        next(err);
+    }
+
+});
+*/
 
 module.exports = router;
