406cc3a2d2e49f2e6802368ad258539410ddc989
Shuba Maxim <pro100stalkerss@gmail.com>
2016-04-14 17:20:41 +0300

    add smal http test

--- test/test.js
+++ test/test.js
@@ -2,14 +2,14 @@ var app = require('../app');
 var should = require('should');
 var supertest = require('supertest');
 
-describe('home page', function () {
-   it('should return home page',
-       function(done) {
+describe('homepage', function () {
+    it('should return home page',
+       function (done) {
         supertest(app)
             .get('/')
             .expect(200)
             .end(function (err, res) {
-                res.status.should
+                res.status.should;
             });
         done();
     });
