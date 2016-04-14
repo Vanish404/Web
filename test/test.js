3193ffd66b8d4a14dc9ec9d3abc3d6a627abc27a
Shuba Maxim <pro100stalkerss@gmail.com>
2016-04-14 19:51:13 +0300

    fix && add new unit test

--- test/test.js
+++ test/test.js
@@ -4,13 +4,23 @@ var supertest = require('supertest');
 
 describe('homepage', function () {
     it('should return home page',
-       function (done) {
-        supertest(app)
-            .get('/')
-            .expect(200)
-            .end(function (err, res) {
-                res.status.should;
-            });
-        done();
-    });
+        function (done) {
+            supertest(app)
+                .get('/')
+                .expect(200)
+                .end(function (err, res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+        });
+    it('should return an err',
+        function (done) {
+            supertest(app)
+                .get('/1234')
+                .expect(404)
+                .end(function (err, res) {
+                    res.status.should.equal(404);
+                    done();
+                });
+        });
 });
