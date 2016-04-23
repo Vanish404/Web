ff7aa37b2c57c3f796db5b40f91ce8ab57656ae7
Shuba Maxim <pro100stalkerss@gmail.com>
2016-04-23 15:12:43 +0300

    add require DB for setinterval

--- app.js
+++ app.js
@@ -7,22 +7,25 @@ var app = express();
 
 app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
-
+var myDat = [];
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use('/', routes);
+
 setInterval(function () {
     var fork = require('child_process').fork;
     var cp = fork('./parser');
-    var myDat = [];
     cp.on('message', function (msgobj) {
         myDat = msgobj;
-        console.log(myDat);
-        // TODO
+        console.log(msgobj);
+        require('./CreateDB');
     });
     cp.send({
         text: 'I send msg'
     });
-}, 180000);
+}, 120000);
+function getPages (callback) {
+    callback(myDat);
+}
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -37,3 +40,4 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+module.exports.getPages = getPages;
